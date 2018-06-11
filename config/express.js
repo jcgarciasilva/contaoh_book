8c933c5fbee84b44c13ef8571be29c2aa5e479f8
jgarcia <joao.garcia-external@alianca.com.br>
2018-06-11 15:53:08 -0300

    Added features for bower configuration

--- config/express.js
+++ config/express.js
@@ -1,20 +1,26 @@
 
 
 var express = require('express');
-var	home	=	require('../app/routes/home');
+var load = require('express-load');
 module.exports = function () {
 	var app = express();
 
 	//	configuração	de	ambiente
-	app.set('port',	3000);
+	app.set('port', 3000);
 
 	//	middleware
 	app.use(express.static('./public'));
 	app.set('view engine', 'ejs');
-	app.set('views','./app/views');
-	
-	//routes
-	home(app);
+	app.set('views', './app/views');
+	app.use(bodyParser.urlencoded({extended:true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
 
+	// load routes
+	load('models', { cwd: 'app' })
+		.then('controllers')
+		.then('routes')
+		.into(app);
 	return app;
+
 };
