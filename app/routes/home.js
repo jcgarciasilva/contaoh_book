2fe3039640af876cd8c6b0c4989d099b76c1a637
jgarcia <joao.garcia-external@alianca.com.br>
2018-06-11 09:00:34 -0300

    initial commit

---
+++ app/routes/home.js
@@ -0,0 +1,7 @@
+
+var controller = require('../controllers/home')();
+module.exports = function (app) {
+    
+    app.get('/', controller.index);
+    app.get('/index', controller.index);
+}   
---
+++ config/express.js
@@ -0,0 +1,20 @@
+
+
+var express = require('express');
+var	home	=	require('../app/routes/home');
+module.exports = function () {
+	var app = express();
+
+	//	configuração	de	ambiente
+	app.set('port',	3000);
+
+	//	middleware
+	app.use(express.static('./public'));
+	app.set('view engine', 'ejs');
+	app.set('views','./app/views');
+	
+	//routes
+	home(app);
+
+	return app;
+};
